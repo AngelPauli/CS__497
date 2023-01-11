913480475556bd85b4e2993dba0f5f26c0f5a83f
AngelPauli <121881502+AngelPauli@users.noreply.github.com>
2023-01-11 12:12:02 -0500

    Add files via upload

---
+++ ex20.js
@@ -0,0 +1,89 @@
+//Read https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics
+// https://www.w3schools.com/js/js_string_templates.asp
+
+let student = {
+    name: "John Doe",
+    age: 24,
+    id: "123456789",
+    gender: "male",
+    address: {
+        number: "56",
+        street: "Main St.",
+        city: "Brooklyn",
+        zip: "12345",
+        state: "NY"
+    },
+    courses: ['CS497', 'CS172', 'CS291', 'CS341'],
+    getAddress: function () {
+        return this.address.number+ " " + this.address.street+ " " + this.address.city+ " " + this.address.zip+ " " + this.address.state;
+    }
+};
+
+document.getElementById("txtName").innerHTML = student.name;
+document.getElementById("txtAge").innerHTML = student.age;
+document.getElementById("txtID").innerHTML = student.id;
+document.getElementById("txtCourses").innerHTML = student.courses;
+document.getElementById("txtAddress").innerHTML = student.getAddress();
+
+
+
+
+
+
+
+let empList =[
+{
+    "fname": "Sharad",
+    "lname": "Volz"
+},
+{
+    "fname": "Constance",
+    "lname": "Dech"
+},
+{
+    "fname": "Myra",
+    "lname": "Comeau"
+},
+{
+    "fname": "Noriess",
+    "lname": "Dabbs"
+},
+{
+    "fname": "Esperanza",
+    "lname": "Shrestha"
+},
+{
+    "fname": "Eugene",
+    "lname": "Barron"
+},
+{
+    "fname": "Petr",
+    "lname": "Ravenelle"
+},
+{
+    "fname": "Cristina",
+    "lname": "Terry"
+},
+{
+    "fname": "Victoria",
+    "lname": "Aresti"
+},
+{
+    "fname": "Byron",
+    "lname": "Yarber"
+},
+];
+
+empList.map((obj) => {
+document.getElementById('empData').innerHTML+=`
+    <tr>
+        <td>${obj.fname}</td>
+        <td>${obj.lname}</td>
+        </tr`;})
+
+
+
+
+
+
+
